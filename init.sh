a59c197ccace05445fe51d28b33d5edbcf0395b6
Tobey Thai <tthai@gn.com>
2025-12-22 14:59:08 -0800

    [init.sh] Setup .gitconfig as .local instead of main .gitconfig

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.gitconfig ~/.gitconfig.local
+sed -i '1s/^/[include]\n\tpath = .\/.gitconfig.local\n/' ~/.gitconfig
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
