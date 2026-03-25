393f425a8fff590a6a772c3e632a203cf33dd9a4
Tobey Thai <tthai@gn.com>
2026-03-24 18:35:50 -0700

    [init.sh] Update logic of adding .gitconfig.local to .gitconfig

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
+#! /bin/bash
+
 ln -s $(pwd)/.gitconfig ~/.gitconfig.local
-sed -i '1s/^/[include]\n\tpath = .\/.gitconfig.local\n/' ~/.gitconfig
+if [ -f ~/.gitconfig ]; then
+  if ! grep -qF "path = ./.gitconfig.local" ~/.gitconfig; then
+    printf '\n[include]\n\tpath = ./.gitconfig.local\n' >> ~.gitconfig
+  fi
+else
+  printf '\n[include]\n\tpath = ./.gitconfig.local\n' >> ~.gitconfig
+fi
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
